be533bbd0c352336abe6f33a0f74956d878dc73f
Syed Sartaj Ali <162809791+Syed-Sartaj-Ali@users.noreply.github.com>
2025-06-11 10:30:21 +0530

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,34 @@
+public class BankAccount {
+    private String accountHolder;
+    private double balance;
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: ₹" + amount);
+        } else {
+            System.out.println("Invalid deposit amount!");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: ₹" + amount);
+        } else {
+            System.out.println("Invalid or insufficient funds!");
+        }
+    }
+}
